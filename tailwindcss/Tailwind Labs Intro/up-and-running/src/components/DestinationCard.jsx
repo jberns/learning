3f803764a200e0f148d37381edaee47fee1f05bc
Josh Bernstein <joshua.bernstein@outlook.com>
2021-08-31 17:03:23 -0600

    Refactored with React and completed 06

---
+++ tailwindcss/Tailwind Labs Intro/up-and-running/src/components/DestinationCard.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export const DestinationCard = (props) => {
+  const { destination } = props;
+  return (
+    <div
+      className='flex items-center overflow-hidden bg-white rounded-lg shadow-lg'
+    >
+      <img
+        className='flex-shrink-0 w-32 h-32'
+        src={destination.imageUrl}
+        alt={destination.imageAlt}
+      />
+      <div className='px-6 py-4'>
+        <h3 className='text-lg font-semibold text-gray-800'>
+          {destination.city}
+        </h3>
+        <p className='text-gray-800'>
+          ${destination.averagePrice} / night average
+        </p>
+        <div className='mt-4'>
+          <a
+            className='text-sm font-semibold text-indigo-500 hover:text-indigo-400'
+            href='#'
+          >
+            Explore {destination.propertyCount} properties
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
